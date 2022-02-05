80d6db1271cee9d5549364463ee104a184784818
AndrewPPinner <andppinner@gmail.com>
2022-02-04 20:20:18 -0500

    Bug Fix: Would not get quest data at times until searching twice

--- src/main/java/com/example/andrew/TarkovSite/ItemDataController.java
+++ src/main/java/com/example/andrew/TarkovSite/ItemDataController.java
@@ -31,7 +31,8 @@
              Browser browser = playwright.chromium().launch();
              Page page = browser.newPage();
              page.navigate(url);
-             page.waitForSelector(".price-wrapper");
+             page.waitForSelector(".quest-name-wrapper");
+             page.mouse().wheel(0,100);
              itemName = page.locator(".item-image").first().getAttribute("alt");
              itemNickname = page.locator(".item-information-wrapper").locator("cite").textContent();
              fleaPrice = page.locator(".flea-wrapper ").textContent();
